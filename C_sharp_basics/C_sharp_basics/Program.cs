6b7e4a0585053c7b2fb8731497f0be3b6a6c9e8f
tbalic <balic.tihana@gmail.com>
2020-11-28 00:42:01 +0100

    Selector menu added to program

--- C_sharp_basics/C_sharp_basics/Program.cs
+++ C_sharp_basics/C_sharp_basics/Program.cs
@@ -6,7 +6,19 @@ namespace C_sharp_basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Odaberite akciju:");
+            Console.WriteLine("1 - Ispis cijele liste");
+            Console.WriteLine("2 - Ispis imena pjesme unosom pripadajućeg rednog broja");
+            Console.WriteLine("3 - Ispis rednog broja pjesme unosom pripadajućeg imena");
+            Console.WriteLine("4 - Unos nove pjesme");
+            Console.WriteLine("5 - Brisanje pjesme po rednom broju");
+            Console.WriteLine("6 - Brisanje pjesme po imenu");
+            Console.WriteLine("7 - Brisanje cijele liste");
+            Console.WriteLine("8 - Uređivanje imena pjesme");
+            Console.WriteLine("9 - Uređivanje rednog broja pjesme");
+            Console.WriteLine("0 - Izlaz iz aplikacije");
+
+
         }
     }
 }
