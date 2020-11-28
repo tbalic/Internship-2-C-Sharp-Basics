33c087e94ed0ea983224bcea2e042b0319e53879
tbalic <balic.tihana@gmail.com>
2020-11-28 01:31:54 +0100

    Added song playlist

--- C_sharp_basics/C_sharp_basics/Program.cs
+++ C_sharp_basics/C_sharp_basics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C_sharp_basics
 {
@@ -18,6 +19,21 @@ namespace C_sharp_basics
             Console.WriteLine("9 - Uređivanje rednog broja pjesme");
             Console.WriteLine("0 - Izlaz iz aplikacije");
 
+            var choiceOfAction = int.Parse(Console.ReadLine());
+
+            var playlist = new Dictionary<int, string>()
+            {
+                { 1, "Chris Isaak - Wicked game" },
+                { 2, "Queen - Bohemian rhapsody" },
+                { 3, "Foo Fighters - Best of you" },
+                { 4, "Oliver - Jubav iza kantuna" },
+                { 5, "Guns N' Roses - Don't cry" },
+                { 6, "Scorpions - Still loving you" },
+                { 7, "Gibonni - Zlatne godine" },
+                { 8, "U2 - I still haven't found what I'm looking for" },
+                { 9, "Dalmatino - Lozje" },
+                { 10, "Kings Of Leon - Use somebody" }
+            };
 
         }
     }
